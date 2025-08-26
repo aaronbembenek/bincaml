ae122e706aebeb503eb714197558b51fe80da73c
rina <k@rina.fyi>
2025-08-26 22:45:38 +1000

    add ast_sandbox.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+
+open Lang.Ast_sandbox
+
+let f x = `BvAdd (x, x)
+
+module Closed = Close(AllOps)
+
+let () =
+  print_endline "Hello, World!";
+  print_endline @@ Int.to_string @@ AllOps.depth_allops @@ f (f (`IntLit 2));
+  print_endline @@ Closed.(show @@ fix @@ f (fix @@ `IntLit 2))
